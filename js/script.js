f4bd06d2dffcb18a1be57b56d181af358ea4258d
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-15 17:39:34 +0100

    Script finito.
    Impostato il comportamento basilare dei menu a tendina. Messa nei commenti una soluzione test alternativa piu complessa, che disabilita l hover al click.

--- js/script.js
+++ js/script.js
@@ -3,23 +3,46 @@
 
 var toggleTrigger = $('.toggle-menu');
 
-var aperto = true;
 
-toggleTrigger.click(function(){
+toggleTrigger.mouseenter (function() {
 
-  $(toggleTrigger).next().hide();
-  $(this).next().toggle();
-  aperto = false;
-  console.log(aperto)
+  $(this).children(".nav-menu").toggle();
+});
+
+toggleTrigger.mouseleave (function() {
+
+  $(this).children(".nav-menu").toggle();
 });
 
 
-if (aperto == true) {
-  toggleTrigger.mouseenter (function() {
-    $(this).next().toggle();
-  });
 
-  toggleTrigger.mouseleave (function() {
-    $(this).next().toggle();
-  });
-}
+//TEST SOLUZIONE AVANZATA
+// Provo a definire delle variabili di controllo per fissare l'apertura dei pannelli al click.
+//Una volta cliccato, la funzione hover è temporaneamente disabilitata fino al click successivo.
+
+// var toggleTrigger = $('.toggle-menu');
+// var aperto = true;
+//
+// toggleTrigger.click(function(){
+//   toggleTrigger.children(".nav-menu").hide();
+//
+// if (aperto == false) {
+//   $(this).children(".nav-menu").hide();
+//
+// } else {
+//   $(this).children(".nav-menu").show();
+//
+// }
+// aperto = !aperto;
+// });
+//
+// toggleTrigger.mouseenter (function() {
+//   if (aperto == true) {
+//     $(this).children(".nav-menu").show();
+//   }
+// });
+// toggleTrigger.mouseleave (function() {
+//   if (aperto == true) {
+//     $(this).children(".nav-menu").hide();
+//   }
+// });
