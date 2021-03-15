a8182f191513d329763c9ac0fbc82c65dc4d17a5
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-15 16:39:10 +0100

    Script - test dei menu a tendina

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 
+// Menu a tendina a scomparsa!
+
+var toggleTrigger = $('.toggle-menu');
+
+var aperto = true;
+
+toggleTrigger.click(function(){
+
+  $(toggleTrigger).next().hide();
+  $(this).next().toggle();
+  aperto = false;
+  console.log(aperto)
+});
+
+
+if (aperto == true) {
+  toggleTrigger.mouseenter (function() {
+    $(this).next().toggle();
+  });
+
+  toggleTrigger.mouseleave (function() {
+    $(this).next().toggle();
+  });
+}
